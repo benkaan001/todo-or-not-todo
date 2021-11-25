c8e138c263e46a95dfd37834c762f3e6ebd67346
Ben Kaan <benkaan001@gmail.com>
2021-11-25 00:27:57 -0600

    test CRUD routes on Postman

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,7 +1,27 @@
 
 
 const getAllTodos = (req,res) => {
-    res.send('Her is everything!')
+    res.send('Here are all the todos!')
 }
 
-module.exports= {getAllTodos};
+const createTodo = (req,res) => {
+    res.json(req.body);
+
+}
+
+const getTodo = (req,res) => {
+    res.json({id: req.params.id})
+}
+
+const updateTodo = (req,res) => {
+    res.json({id:req.params.id})
+}
+
+const deleteTodo = (req,res) => {
+    res.json({id:req.params.id})
+}
+
+
+
+
+module.exports= {getAllTodos, createTodo, getTodo,updateTodo,deleteTodo};
--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const {getAllTodos} = require('../controllers/todos')
+const {
+    getAllTodos, 
+    updateTodo, 
+    deleteTodo, 
+    getTodo, 
+    createTodo } = require('../controllers/todos')
 
 
 router.route('/').get(getAllTodos);
+router.route('/').post(createTodo);
+router. route('/:id').get(getTodo);
+router.route('/:id').patch(updateTodo);
+router.route('/:id').delete(deleteTodo);
+
 
 
 module.exports = router;
