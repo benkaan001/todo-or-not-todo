860fb718a326748ef6f0e23f021277e66f54a94e
Ben Kaan <benkaan001@gmail.com>
2021-11-24 23:27:44 -0600

    set up/test router

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('say yes to MangoDB!!!')
+const express = require('express');
+const { send } = require('process');
+const app = express();
+
+
+app.get('/', (req,res) => {
+    res.send(`<h1>test</h1>`);
+})
+
+
+
+const port = process.env.PORT || 3000;
+app.listen (port, () =>{
+    console.log(`=> 🌍 => server is listening on PORT ${port}......`);
+})
