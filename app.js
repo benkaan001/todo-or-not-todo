03c92055bf45b92d8b64a3b2902558e22b8d98f9
Ben Kaan <benkaan001@gmail.com>
2021-11-25 01:32:10 -0600

    set up db connection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const todos = require('./routes/todos')
+const todos = require('./routes/todos');
 
 // add middleware -- without this we don't have body
 app.use(express.json())
@@ -12,7 +15,19 @@ app.use(express.json())
 app.use('/api/v1/todos', todos)
 
 
-const port = process.env.PORT || 3000;
-app.listen (port, () =>{
-    console.log(`=> 🌍 => server is listening on PORT ${port}......`);
-})
+const port = 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen (port, () =>{
+            console.log(`=> 🌍 => server is listening on PORT ${port}......`);
+        })
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+start();
+
+
